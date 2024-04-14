9d1e209bb025a5d95f1a46ab49865f2c7d6667eb
Travis Umbel <Umbelth94@gmail.com>
2024-04-14 17:47:02 -0500

    Added asset cache strategy and routes

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -7,6 +7,7 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Cache strategy for HTML pages
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -19,12 +20,35 @@ const pageCache = new CacheFirst({
   ],
 });
 
+// URLs to be pre-cached for faster access
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// Cache strategy for assets (Javascript, CSS, images)
+const assetCache = new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+// Register route for navigation request
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// Register route for assets
+registerRoute(
+  // Customize this route to match your assets URL pattern
+  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'image',
+  assetCache
+);
+
+
+
 registerRoute();
